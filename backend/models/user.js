e9dfd4a8aa9c19b96ec6397062fdd86d2271e11a
Kye Choong <kyecdesign@gmail.com>
2023-12-17 15:17:03 +0900

    first init

---
+++ backend/models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {type: String},
+    googleID: {
+        type: String,
+        required : true
+    },
+    email: {type: String},
+    avatar: {type: String}
+
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('User', userSchema);
---
+++ backend/routes/index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var passport = require('passport');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/auth/google', passport.authenticate(
+  // Which passport strategy is being used?
+  'google',
+  {
+    // Requesting the user's profile and email
+    scope: ['profile', 'email'],
+    // Optionally forice pick account every time
+    // promt: "select_account"
+  }
+));
+
+// Google OaAuth callback route
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/', // May need to update later
+    failureRedirect: '/'
+  }
+));
+
+// OAuth logout route
+router.get('/logout', function(req, res){
+  req.logout(function() {
+    res.redirect('/');
+  });
+});
+
+module.exports = router;
